17ca9be881544a97f19a10d4305664f7b6590fad
Gordon <blu3sag3@gmail.com>
2018-11-02 15:06:41 -0700

    Everything

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{   //Used to set player movement speed
+    public float moveSpeed;
+
+    //Gets components from Unity
+    private Rigidbody2D myRigidbody;
+    private Collider2D collide;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        collide = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        walk();
+    }
+
+    void walk()
+    {
+        float movement = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        myRigidbody.velocity = new Vector2(movement, myRigidbody.velocity.y);
+    }
+
+}
