a0ef24de5b36c57d162f837a6ada409a025e19fe
Gordon <blu3sag3@gmail.com>
2018-11-09 13:44:04 -0800

    Everything

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {   //Used to set player movement speed
     public float moveSpeed;
-
+    public int playerJumpPower = 1250;
+    public string midjump = "n";
     //Gets components from Unity
     private Rigidbody2D myRigidbody;
     private Collider2D collide;
@@ -25,6 +26,19 @@ public class PlayerController : MonoBehaviour
     {
         float movement = Input.GetAxisRaw("Horizontal") * moveSpeed;
         myRigidbody.velocity = new Vector2(movement, myRigidbody.velocity.y);
+        if (Input.GetButtonDown("Jump") && midjump == "n")
+        {
+            Jump();
+            midjump = "y";
+        }
+        if (GetComponent<Rigidbody2D>().velocity.y == 0)
+        {
+            midjump = "n";
+        }
     }
 
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+    }
 }
